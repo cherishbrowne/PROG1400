eb6f618d1babafca2940d2c8a65fbab36077c928
cherishbrowne <54998398+cherishbrowne@users.noreply.github.com>
2020-01-09 12:18:09 -0400

    changed to display a number

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,7 +1,12 @@
 
     class  HelloWorld{
         public static void main(String[] args) {
+
+            int num;
+
+            num = 123456;
+
             // prints out hello
-            System.out.println("Hello World!");
+            System.out.println("num = " + num);
         }
 }
