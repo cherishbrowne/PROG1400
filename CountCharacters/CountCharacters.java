70ff9790d0adb9e0ae4799062e5ed3a9883db93b
cherishbrowne <54998398+cherishbrowne@users.noreply.github.com>
2020-01-23 11:34:52 -0400

    w/ out magic numbers

---
+++ CountCharacters/CountCharacters.java
@@ -0,0 +1,4 @@
+package CountCharacters;
+
+public class CountCharacters {
+}
--- FinalGrade/FinalGrade.java
+++ FinalGrade/FinalGrade.java
@@ -12,7 +12,7 @@ public class FinalGrade {
         Scanner s = new Scanner(System.in);
 
         // loop 6 times to get all assignment grades
-        for (int i=0; i<6; i++) {
+        for (int i=0; i<assignments.length; i++) {
             System.out.print("Enter grade for assignment " + (i+1) + ": ");
             assignments[i] = s.nextFloat();
         }
@@ -20,7 +20,7 @@ public class FinalGrade {
         // find the lowest grade
         float lowest = 100.0f;
         int lowestIndex = 0;
-        for (int i=0; i<6; i++) {
+        for (int i=0; i<assignments.length; i++) {
             if (assignments[i] < lowest) {
                 lowest = assignments[i];
                 lowestIndex = i;
@@ -28,25 +28,30 @@ public class FinalGrade {
         }
 
         // display all grades
-        for (int i=0; i<6; i++) {
+        for (int i=0; i<assignments.length; i++) {
             if (i == lowestIndex) {
-            System.out.println("Assignment " + (i+1) + ": " + assignments[i] + " (lowest)"
-            );
-        } else {
-            System.out.println(
-                    "Assignment " + (i + 1) + ": " + assignments[i]
-            );
+                System.out.println(
+                        "Assignment " + (i+1) + ": " +
+                                assignments[i] + " (lowest)"
+                );
+            } else {
+                System.out.println(
+                        "Assignment " + (i + 1) + ": " +
+                                assignments[i]);
+            }
         }
 
-            // calculate the final grade
-            float finalGrade = 0.0f;
+        // calculate the final grade
+        float finalGrade = 0.0f;
 
-            for (int i=0; i<6; i++) {
-                if (i != lowestIndex) {
-                    finalGrade += assignments[i];
-                }
+        for (int i=0; i<assignments.length; i++) {
+            if (i != lowestIndex) {
+                finalGrade += assignments[i];
             }
-        System.out.println("Your final grade is: " + finalGrade);
+        }
+        // subtracted one to account for optional assignment
+        finalGrade /= (assignments.length - 1);
 
+        System.out.println("Your final grade is: " + finalGrade);
     }
-}}
+}
