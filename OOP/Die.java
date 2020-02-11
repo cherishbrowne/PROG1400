642096d0a50aa51fbc6d2f3e73cd4c4a90db7546
cherishbrowne <54998398+cherishbrowne@users.noreply.github.com>
2020-02-11 13:43:32 -0400

    OOP Game

---
+++ OOP/Die.java
@@ -0,0 +1,12 @@
+package OOP;
+
+import java.util.Random;
+
+public class Die {
+    int value;
+
+    public void roll() {
+        Random random = new Random();
+        value = random.nextInt(6) + 1;
+    }
+}
