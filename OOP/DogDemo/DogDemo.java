41ffbc196d5cfd131aded0c4801cad2b2622b077
cherishbrowne <54998398+cherishbrowne@users.noreply.github.com>
2020-02-11 14:25:17 -0400

    Dog Demo

--- OOP/DogDemo/DogDemo.java
+++ OOP/DogDemo/DogDemo.java
@@ -5,9 +5,26 @@ class Dog {
     int numLegs;
     String breed;
     boolean fleas;
+    boolean hungry;
+
+    boolean isHungry() {
+        return hungry;
+    }
+
+    void eat() {
+        hungry = false;
+    }
+
+    void play() {
+        hungry = true;
+    }
 }
 
 public class DogDemo {
+
+    public static void dogEat(Dog dog) {
+        dog.hungry = false;
+    }
 
     public static void main(String[] args) {
 
@@ -16,6 +33,20 @@ public class DogDemo {
         spot.numLegs = 3;
         spot.breed = "Collie";
         spot.fleas = true;
+
+        System.out.println("Name: " + spot.name);
+        System.out.println("Breed: " + spot.breed);
+        System.out.println("# of legs: " + spot.numLegs);
+        System.out.println("Has fleas? " + spot.fleas);
+
+        spot.play();
+        System.out.println("Hungry? " + spot.hungry);
+
+        if (spot.isHungry()) {
+            spot.eat();
+            dogEat(spot);
+        }
+        System.out.println("Hungry? " + spot.hungry);
 
     }
 
